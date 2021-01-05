f280ec2eca57719191c2d2e8c76183f8047d9979
Jamie Sullivan <jkcrawshaw@gmail.com>
2021-01-04 19:46:14 -0500

    adds post to /shortUrl

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
 mongoose.connect('mongodb://localhost/urlShortener', {
-    useNewUrlParser: true, useUnifiedTopology: true
+  useNewUrlParser: true, useUnifiedTopology: true
 })
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.post('/shortUrls', (req, res) => {
-
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullURL })
+    res.redirect('/')
 })
 app.listen(process.env.PORT || 5000);
 
